3a42e4d6256f16e522c4c8d3082c065fa8215315
mohan-warrior <99632496+mohan-warrior@users.noreply.github.com>
2024-05-31 18:08:46 +0000

    cnversion_Op_Done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber); 
-console.log(valueInNumber);
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
 
 // after conversion output values
 //"33" => 33
@@ -18,7 +18,7 @@ console.log(valueInNumber);
 let isLoggedIn= ""
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1=>true; 0=>false
 //"" => false
@@ -28,5 +28,47 @@ console.log(booleanIsLoggedIn);
 let someNumber =44
 
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *************************Operations****************************
+
+let value = 3 
+let negValue =-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Mohan"
+
+let str3= str1+str2
+// console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+
+//above 4 lines of code will not be accepted in big companies 
+// console.log((3+4)*5%3);//the code need to be clear by using brackets whereever needed
+
+console.log(+true); //1
+console.log(+"");//0
+
+let num1, num2, num3 
+num1=num2=num3=2+2 //not a good practice 
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
+
+let gameCounter1 =200
+++gameCounter1
+console.log(gameCounter1);
