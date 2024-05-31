8364b6300bcddd04c7a48c26cc471735a5591dac
mohan-warrior <99632496+mohan-warrior@users.noreply.github.com>
2024-05-31 05:52:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treats all JS code as newer version 
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3) //focus on prettier code writing
+
+console.log("MOhan")
+
+let name ="Mohan"
+let age =20
+let isLoggedIn=false
+let state
+
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean =>true/false
+//null =>standalone value ex: like you thermo is not working then it should give null value not undefined
+//undefined=> value not assigned 
+//symbol => shows uniqueness
+
+
+//object 
+
+console.log(typeof null) // ans: object->flaws of JS 
+console.log(typeof undefined) // ans: undefined(type)
