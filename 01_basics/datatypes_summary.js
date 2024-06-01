396b8488b04d0f9afb7793bcba872f44bd3e5ac1
mohan-warrior <99632496+mohan-warrior@users.noreply.github.com>
2024-06-01 18:16:42 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,42 @@
+// primitive 
+//7 types :String,number, Boolean, null,
+//  undefined, Symbol, BigInt
+
+const score = 100 
+const scoreValue =100.3
+
+const isLoggedIn =false 
+const outsideTemp =null
+let userEmail;//value will be undefined here
+
+
+const id = Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber=345678347784844849n
+
+
+//Reference (Non-primitive)
+//Array ,Objects, Functions
+
+const heros=["shaktiman","naagraj","doga"];
+
+//object {}
+let myObj={
+    name:"Mohan",
+    age: 22,
+}
+
+
+const myFunction =function (){
+    console.log("Hello World");
+}
+
+console.log(typeof null); //object
+console.log(typeof myFunction);//function
+
+console.log(typeof anotherId);//symbol
+
+//https://262.ecma-international.org/5.1/#sec11.4.3
